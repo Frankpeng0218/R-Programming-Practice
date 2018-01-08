d9a0fa56e6371d8e1fdef48eb66981b66eda8e8c
Cai, Yun Ting <leotsai0119@gmail.com>
2018-01-08 22:48:19 +0800

    01/08

--- read.R
+++ read.R
@@ -20,7 +20,7 @@ for(i in 1:5) {
         ps[[i]] <- c(start[1:9], start[10:34] + 38*(i-1), start[35] + (i-1))
         pe[[i]] <- c(end[1:9], end[10:34] + 38*(i-1), start[35] + (i-1))
         pp[[i]] <- mapply(fwf_positions, ps[[i]], pe[[i]], SIMPLIFY = FALSE, USE.NAMES = FALSE)
-        pp[[i]] <- do.call(rbind, pp[[i]])
+        pp[[i]] <- rbindlist(pp[[i]])
         pp[[i]]$col_names <- c(codebook$變項名稱[1:34], "OMG")
         #read all the files in one directory
         filenames <- dir(path)
@@ -32,3 +32,4 @@ for(i in 1:5) {
 }
 #199232
 myData <- data[OMG %in% c("Y", "I", "R") & COUNTY != 28 & COUNTY != 29]
+rm(ps, pe, pp, dd, data)
